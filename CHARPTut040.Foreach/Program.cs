beb2e779f0e42ed8d82a759d25579ce7df292200
Abdou144 <mohamedabdou1444@gmail.com>
2024-05-01 13:59:53 +0300

    Add project files.

---
+++ CHARPTut040.Foreach/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace CHARPTut040.Foreach
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = new []{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Foreach(number);
+            Console.ReadKey();
+        }
+        static void Foreach<T>( IEnumerable<T> arr)
+        {
+            IEnumerator<T> enumerator = arr.GetEnumerator();
+            IDisposable disposable;
+            try
+            {
+                while (enumerator.MoveNext())
+                {
+                    Console.WriteLine(enumerator.Current);
+
+                }
+            }
+            finally
+            {
+                disposable = (IDisposable)enumerator;
+                disposable.Dispose();
+            }
+        }
+    }
+}
---
+++ CHARPTut040.Yield/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CHARPTut040.Yield
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in Generatev3())
+            {
+                Console.Write(item);
+                //MOVE NEXT GO TO BODY UNTIL RUN AGAINT THE FIRST YIELD AND YEILD DEFINE THE CURRENT VALUE AND RETURN TO THE CALLER
+                //AND SCEOND MOVE NEXT WILL GO TO NEXT LINE  
+
+            }
+            Console.ReadKey();
+        }
+        public static IEnumerable<int> Generatev2()
+        {
+            for(int i=1; i<5; i++)
+            {
+                yield return i;
+            }
+        }
+        public static IEnumerable<int> Generatev3()
+        {
+            
+                yield return 1;
+                yield return 2;
+                yield return 3;
+                yield return 4;
+                yield return 5;
+                yield break;
+        }
+    }
+}
